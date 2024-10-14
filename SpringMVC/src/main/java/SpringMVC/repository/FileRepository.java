63cd6cdcb6a265f4f07a9aa743f9095eef4cf20f
duc <duc53044@donga.edu.vn>
2024-10-14 15:00:44 +0700

    xoa

--- SpringMVC/src/main/java/SpringMVC/repository/FileRepository.java
+++
@@ -1,7 +0,0 @@
-package SpringMVC.repository;
-
-import SpringMVC.entity.FileModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FileRepository extends JpaRepository<FileModel,Long> {
-}
