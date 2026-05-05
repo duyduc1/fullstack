e9f41599e74743580f9800e407abb8b0e4a28d9a
ducvd <vuduyduc550@gmail.com>
2026-05-05 22:25:05 +0700

    fuckkkk

--- nest-task-ecs/nest-order-api/src/config/database.config.ts
+++ nest-task-ecs/nest-order-api/src/config/database.config.ts
@@ -1,10 +1,10 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('database', () => ({
-  host: process.env.RDS_ENDPOINT || 'localhost',
+  host: process.env.RDS_ENDPOINT || 'database-1.cnmyaogcoxlf.us-east-2.rds.amazonaws.com',
   port: parseInt(process.env.RDS_PORT, 10) || 3306,
   username: process.env.RDS_USERNAME || 'root',
-  password: process.env.RDS_PASSWORD || '',
+  password: process.env.RDS_PASSWORD || 'M1aT4Tt7aeXc',
   name: process.env.RDS_DB_NAME || 'testdb',
   synchronize: process.env.DB_SYNCHRONIZE === 'true',
   logging: process.env.DB_LOGGING === 'true',
