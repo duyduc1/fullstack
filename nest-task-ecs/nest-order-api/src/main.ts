2a251b677b1e64439911b1745a3d412f4a15d58a
duyduc1 <106147757+duyduc1@users.noreply.github.com>
2026-06-01 21:38:36 +0700

    Update main.ts

--- nest-task-ecs/nest-order-api/src/main.ts
+++ nest-task-ecs/nest-order-api/src/main.ts
@@ -42,7 +42,7 @@ async function bootstrap() {
   await app.listen(port);
 
   console.log(`🚀 App running:     http://localhost:${port}/api/v1`);
-  console.log(`📖 Swagger docs:    http://localhost:${port}/api/doc123`);
+  console.log(`📖 Swagger docs:    http://localhost:${port}/api/rundditroi`);
 }
 
 bootstrap();
