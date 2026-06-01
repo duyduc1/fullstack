79fbf4bd59642b75c395ebfb796d922a8932244a
ducvd <vuduyduc550@gmail.com>
2026-06-01 21:32:43 +0700

    ..

--- nest-task-ecs/nest-order-api/src/main.ts
+++ nest-task-ecs/nest-order-api/src/main.ts
@@ -42,7 +42,7 @@ async function bootstrap() {
   await app.listen(port);
 
   console.log(`🚀 App running:     http://localhost:${port}/api/v1`);
-  console.log(`📖 Swagger docs:    http://localhost:${port}/api/docsssssssssss333`);
+  console.log(`📖 Swagger docs:    http://localhost:${port}/api/docssssssss222333`);
 }
 
 bootstrap();
